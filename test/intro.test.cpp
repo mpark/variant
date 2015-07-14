26e485f90f8b860960a3c9be8f20ff9579f66002
Michael Park <mcypark@gmail.com>
2015-07-14 15:53:06 -0400

    <test/typical.test.cpp> -> <test/intro.test.cpp> and elaborate a little

---
+++ test/intro.test.cpp
@@ -0,0 +1,24 @@
+#include <mpark/variant.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(Variant, Intro) {
+  mpark::variant<int, float> v(1.1f);
+  // Check `v`.
+  EXPECT_EQ(typeid(float), v.type());
+  EXPECT_FLOAT_EQ(1.1f, mpark::get<float>(v));
+  // Change type.
+  v = 42;
+  // Check `v`.
+  EXPECT_EQ(typeid(int), v.type());
+  EXPECT_EQ(42, mpark::get<int>(v));
+  // Copy from `v`.
+  mpark::variant<int, float> w = v;
+  // Check `w`.
+  EXPECT_EQ(typeid(int), w.type());
+  EXPECT_EQ(42, mpark::get<int>(w));
+
+  // mpark::get<double>(v);  // ill-formed.
+
+  EXPECT_THROW(mpark::get<float>(v), mpark::bad_variant_access);
+}
