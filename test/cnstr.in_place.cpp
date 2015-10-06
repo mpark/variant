68588f649b9b55e9bfee4e7501714586ff814c91
Michael Park <mcypark@gmail.com>
2015-10-05 22:40:54 -0400

    Added tests for reference storing variants' in-place constructors.

--- test/cnstr.in_place.cpp
+++ test/cnstr.in_place.cpp
@@ -13,11 +13,26 @@ TEST(InPlaceCnstr, IndexDirect) {
   EXPECT_EQ(42, mpark::get<I{}>(v));
 }
 
+TEST(InPlaceCnstr, IndexDirectDuplicate) {
+  using I = meta::size_t<0>;
+  mpark::variant<int, int> v(mpark::in_place<I{}>, 42);
+  EXPECT_EQ(I{}, v.index());
+  EXPECT_EQ(42, mpark::get<I{}>(v));
+}
+
 TEST(InPlaceCnstr, IndexConversion) {
   using I = meta::size_t<1>;
   mpark::variant<int, std::string> v(mpark::in_place<I{}>, "42");
   EXPECT_EQ(I{}, v.index());
   EXPECT_EQ("42"s, mpark::get<I{}>(v));
+}
+
+TEST(InPlaceCnstr, IndexConversionDuplicateRef) {
+  using I = meta::size_t<0>;
+  std::ostringstream strm;
+  mpark::variant<std::ostream &, std::ostream &> v(mpark::in_place<I{}>, strm);
+  EXPECT_EQ(I{}, v.index());
+  EXPECT_EQ(&strm, &mpark::get<I{}>(v));
 }
 
 TEST(InPlaceCnstr, IndexInitializerList) {
@@ -34,11 +49,29 @@ TEST(InPlaceCnstr, TypeDirect) {
   EXPECT_EQ("42"s, mpark::get<T>(v));
 }
 
+TEST(InPlaceCnstr, TypeDirectRef) {
+  using T = int &;
+  int expected = 42;
+  mpark::variant<int &, std::ostream &> v(mpark::in_place<T>, expected);
+  EXPECT_EQ(0u, v.index());
+  auto &actual = mpark::get<T>(v);
+  EXPECT_EQ(expected, actual);
+  EXPECT_EQ(&expected, &actual);
+}
+
 TEST(InPlaceCnstr, TypeConversion) {
   using T = int;
   mpark::variant<int, std::string> v(mpark::in_place<T>, 42.5);
   EXPECT_EQ(0u, v.index());
   EXPECT_EQ(42, mpark::get<T>(v));
+}
+
+TEST(InPlaceCnstr, TypeConversionRef) {
+  using T = std::ostream &;
+  std::ostringstream strm;
+  mpark::variant<int, std::ostream &> v(mpark::in_place<T>, strm);
+  EXPECT_EQ(1u, v.index());
+  EXPECT_EQ(&strm, &mpark::get<T>(v));
 }
 
 TEST(InPlaceCnstr, TypeInitializerList) {
