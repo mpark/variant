84094579928730e8c6afb94fb6056c013a4860fc
Michael Park <mcypark@gmail.com>
2015-10-29 20:28:08 -0500

    Released under Boost Software License.

--- include/variant/monostate.hpp
+++ include/variant/monostate.hpp
@@ -1,3 +1,8 @@
+// Copyright Michael Park 2015
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef VARIANT_MONOSTATE_HPP
 #define VARIANT_MONOSTATE_HPP
 
