756fffc25f5e3317428a50e08e47774da9711e93
Michael Park <mpark@apache.org>
2017-01-09 03:16:55 -0800

    Added `install` targets and `find_package` support.

--- test/run_libcxx_tests.sh
+++ test/run_libcxx_tests.sh
@@ -1,3 +1,11 @@
+# MPark.Variant
+#
+# Copyright Michael Park, 2015-2016
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
 #!/usr/bin/env bash
 
 set -e
