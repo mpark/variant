2365da7c7e58f3b66a609a77945b843e9651485b
Michael Park <mcypark@gmail.com>
2017-08-07 16:10:32 -0700

    Added anthonyw.

---
+++ dependencies/anthonyw.cmake
@@ -0,0 +1,29 @@
+# MPark.Variant
+#
+# Copyright Michael Park, 2015-2017
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+include(ExternalProject)
+
+ExternalProject_Add(anthonyw
+  URL https://bitbucket.org/anthonyw/variant/get/d2df0f0039dd.zip
+  GIT_SHALLOW 1
+  TIMEOUT 120
+  SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/dependencies/anthonyw"
+  CONFIGURE_COMMAND "" # Disable configure step
+  BUILD_COMMAND ""     # Disable build step
+  INSTALL_COMMAND ""   # Disable install step
+  TEST_COMMAND ""      # Disable test step
+  UPDATE_COMMAND ""    # Disable source work-tree update
+)
+
+ExternalProject_Get_Property(anthonyw SOURCE_DIR)
+set(anthonyw_variant_INCLUDE_DIRS ${SOURCE_DIR})
+
+function(anthonyw_add_dataset dataset range)
+  metabench_add_dataset(${dataset} anthonyw.cpp.erb ${range} NAME anthonyw)
+  target_include_directories(${dataset} PUBLIC ${anthonyw_variant_INCLUDE_DIRS})
+  add_dependencies(${dataset} anthonyw)
+endfunction()
