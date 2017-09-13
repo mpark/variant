0a931efe60c7e3836ec43a85d549e8d43a2a6d53
Michael Park <mcypark@gmail.com>
2017-09-13 15:51:22 -0700

    Add Wandbox uploader.

--- support/single-header.py
+++ support/single-header.py
@@ -1,6 +1,8 @@
+#! /usr/bin/env python
+
 # MPark.Variant
 #
-# Copyright Michael Park, 2015-2017
+# Copyright Michael Park, 2017
 #
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
