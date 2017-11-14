21ed03e5ed55b847a0999ac491ebeef8cd8c45d5
Michael Park <mcypark@gmail.com>
2017-11-13 17:52:32 -0800

    Workaround an MSVC issue.

--- test/json.cpp
+++ test/json.cpp
@@ -9,35 +9,41 @@
 
 #include <map>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include <gtest/gtest.h>
 
-struct JsonIsh : mpark::variant<bool, int, std::string, std::vector<JsonIsh>> {
-  using variant::variant;
+struct JsonIsh {
+  JsonIsh(bool b) : data(b) {}
+  JsonIsh(int i) : data(i) {}
+  JsonIsh(std::string s) : data(std::move(s)) {}
+  JsonIsh(std::vector<JsonIsh> v) : data(std::move(v)) {}
+
+  mpark::variant<bool, int, std::string, std::vector<JsonIsh>> data;
 };
 
 TEST(Variant, Bool) {
-  JsonIsh json = true;
-  EXPECT_TRUE(mpark::get<bool>(json));
-  json = false;
-  EXPECT_FALSE(mpark::get<bool>(json));
+  JsonIsh json_ish = true;
+  EXPECT_TRUE(mpark::get<bool>(json_ish.data));
+  json_ish = false;
+  EXPECT_FALSE(mpark::get<bool>(json_ish.data));
 }
 
 TEST(Variant, Int) {
-  JsonIsh json = 42;
-  EXPECT_EQ(42, mpark::get<int>(json));
+  JsonIsh json_ish = 42;
+  EXPECT_EQ(42, mpark::get<int>(json_ish.data));
 }
 
 TEST(Variant, String) {
-  JsonIsh json = std::string("hello");
-  EXPECT_EQ("hello", mpark::get<std::string>(json));
+  JsonIsh json_ish = std::string("hello");
+  EXPECT_EQ("hello", mpark::get<std::string>(json_ish.data));
 }
 
 TEST(Variant, Array) {
-  JsonIsh json = std::vector<JsonIsh>{true, 42, std::string("world")};
-  const auto &array = mpark::get<std::vector<JsonIsh>>(json);
-  EXPECT_TRUE(mpark::get<bool>(array[0]));
-  EXPECT_EQ(42, mpark::get<int>(array[1]));
-  EXPECT_EQ("world", mpark::get<std::string>(array[2]));
+  JsonIsh json_ish = std::vector<JsonIsh>{true, 42, std::string("world")};
+  const auto &array = mpark::get<std::vector<JsonIsh>>(json_ish.data);
+  EXPECT_TRUE(mpark::get<bool>(array[0].data));
+  EXPECT_EQ(42, mpark::get<int>(array[1].data));
+  EXPECT_EQ("world", mpark::get<std::string>(array[2].data));
 }
