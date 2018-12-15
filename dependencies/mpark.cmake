0ee8eebdf6de896745f61eb14ede8b84d138b963
Michael Park <mcypark@gmail.com>
2018-12-14 18:05:15 -0800

    Added the `mpark-dev` branch.

--- dependencies/mpark.cmake
+++ dependencies/mpark.cmake
@@ -19,14 +19,35 @@ if (MPARK_VARIANT_INCLUDE_MPARK_BENCHMARKS)
     UPDATE_COMMAND ""    # Disable source work-tree update
   )
 
+  ExternalProject_Add(mpark-dev
+    GIT_REPOSITORY https://github.com/mpark/variant.git
+    GIT_TAG dev
+    GIT_SHALLOW 1
+    TIMEOUT 120
+    SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/dependencies/mpark-dev"
+    CONFIGURE_COMMAND "" # Disable configure step
+    BUILD_COMMAND ""     # Disable build step
+    INSTALL_COMMAND ""   # Disable install step
+    TEST_COMMAND ""      # Disable test step
+    UPDATE_COMMAND ""    # Disable source work-tree update
+  )
+
   ExternalProject_Get_Property(mpark SOURCE_DIR)
   set(mpark_INCLUDE_DIRS ${SOURCE_DIR}/include)
+
+  ExternalProject_Get_Property(mpark-dev SOURCE_DIR)
+  set(mpark_dev_INCLUDE_DIRS ${SOURCE_DIR}/include)
 
   function(mpark_add_compile_benchmark dataset repeat range)
     metabench_add_dataset(${dataset} compile.mpark.cpp.erb ${range}
                           NAME mpark ENV "{repeat: ${repeat}}")
     target_include_directories(${dataset} PUBLIC ${mpark_INCLUDE_DIRS})
     add_dependencies(${dataset} mpark)
+
+    metabench_add_dataset(${dataset}-dev compile.mpark.cpp.erb ${range}
+                          NAME mpark-dev ENV "{repeat: ${repeat}}")
+    target_include_directories(${dataset}-dev PUBLIC ${mpark_dev_INCLUDE_DIRS})
+    add_dependencies(${dataset}-dev mpark-dev)
   endfunction()
 
   function(mpark_add_execute_benchmark benchmark)
@@ -35,6 +56,12 @@ if (MPARK_VARIANT_INCLUDE_MPARK_BENCHMARKS)
     target_link_libraries(${benchmark} PUBLIC benchmark_main)
     add_test(${benchmark} ${benchmark} --benchmark_color=true)
     add_dependencies(${benchmark} mpark)
+
+    add_executable(${benchmark}-dev execute.mpark.cpp)
+    target_include_directories(${benchmark}-dev PUBLIC ${mpark_dev_INCLUDE_DIRS})
+    target_link_libraries(${benchmark}-dev PUBLIC benchmark_main)
+    add_test(${benchmark}-dev ${benchmark}-dev --benchmark_color=true)
+    add_dependencies(${benchmark}-dev mpark-dev)
   endfunction()
 else()
   function(mpark_add_compile_benchmark)
